322bf4b838ae2f97cad6077852f43e2685e1262c
syshack <syshack@163.com>
2013-03-26 17:53:13 +0800

    Update urllib2-put.py

--- urllib2-put.py
+++ urllib2-put.py
@@ -2,5 +2,5 @@ import urllib2
 opener = urllib2.build_opener(urllib2.HTTPHandler)
 request = urllib2.Request('http://example.org', data='your_put_data')
 request.add_header('Content-Type', 'your/contenttype')
-request.get_method = lambda: 'PUT'
+request.get_method = lambda: 'PUT' //用这种可以定义任何的http方法，比如很多网站需要的put，update//
 url = opener.open(request)
